cc4affd9b244834f380166854bf685408f03456b
Mohamed Sadek <mohamed.sadek1011@gmail.com>
2020-10-14 18:56:48 +0200

    added spaces in the code so it is easier to read

--- stats.c
+++ stats.c
@@ -30,8 +30,8 @@
 #define SIZE (40)
 
 
-void main() {
-
+void main()
+{
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
@@ -54,102 +54,104 @@ void main() {
 
 void print_statistics (unsigned char minimum,unsigned char maximum,unsigned char mean, unsigned char median)
 {
-printf("The array statistics:\n");
-printf("The minimum number is : %d\n",minimum);
-printf("The maximum number is : %d\n",maximum);
-printf("The median is : %d\n",median);
-printf("The mean is : %d\n",mean);
+  printf("The array statistics:\n");
+  printf("The minimum number is : %d\n",minimum);
+  printf("The maximum number is : %d\n",maximum);
+  printf("The median is : %d\n",median);
+  printf("The mean is : %d\n",mean);
 }
 
 void print_array (unsigned char* array_adress,unsigned char array_length)
 {
-printf("%d elements array:\n",array_length);
+  printf("%d elements array:\n",array_length);
+
 for (unsigned char i = 1; i <= array_length ; i++)
 {
-if ( i == 1 )
-printf("{%d, ",*array_adress);
-else if ( i == array_length )
-printf("%d}\n",*array_adress);
-else
-printf("%d, ",*array_adress);
-array_adress++;
+  if ( i == 1 )
+    printf("{%d, ",*array_adress);
+  else if ( i == array_length )
+    printf("%d}\n",*array_adress);
+  else
+    printf("%d, ",*array_adress);
+  array_adress++;
 }
+
 }
 
 unsigned char find_median (unsigned char* array_adress,unsigned char array_length)
 {
-unsigned char median;
-sort_array(array_adress,array_length);
-if ( array_length % 2 == 0 )
-{
-array_adress += (array_length/2);
-median = *array_adress;
-array_adress++;
-median = ( median + *array_adress ) / 2;
-}
-else
-{
-array_adress += (array_length/2+1);
-median = *array_adress;
-}
-return median;
+  unsigned char median;
+  sort_array(array_adress,array_length);
+  if ( array_length % 2 == 0 )
+  {
+    array_adress += (array_length/2);
+    median = *array_adress;
+    array_adress++;
+    median = ( median + *array_adress ) / 2;
+  }
+  else
+  {
+    array_adress += (array_length/2+1);
+    median = *array_adress;
+  }
+  return median;
 }
 
 unsigned char find_mean (unsigned char* array_adress,unsigned char array_length)
 {
-unsigned short int sum = 0;
-unsigned char mean;
+  unsigned short int sum = 0;
+  unsigned char mean;
 
-for ( unsigned char i = 1 ; i <= array_length ; i++ )
-{
-sum += *array_adress;
-array_adress++; 
-}
+  for ( unsigned char i = 1 ; i <= array_length ; i++ )
+  {
+    sum += *array_adress;
+    array_adress++; 
+  }
 
-mean = sum/array_length;
-return mean;
+  mean = sum/array_length;
+  return mean;
 
 }
 
 unsigned char find_maximum (unsigned char* array_adress,unsigned char array_length)
 {
-sort_array(array_adress,array_length);
-return *array_adress;
+  sort_array(array_adress,array_length);
+  return *array_adress;
 }
 
 unsigned char find_minimum (unsigned char* array_adress,unsigned char array_length)
 {
-sort_array(array_adress,array_length);
-array_adress += (array_length-1);
-return *array_adress;
+  sort_array(array_adress,array_length);
+  array_adress += (array_length-1);
+  return *array_adress;
 
 }
 
 void sort_array (unsigned char* array_adress,unsigned char array_length)
 {
-unsigned char* prev_element_adress = array_adress;
-unsigned char* current_element_adress = array_adress+1;
-for ( unsigned char i = 1 ; i <= (array_length-1)  ; i++ )
-{
-prev_element_adress = array_adress;
-current_element_adress = array_adress+1;
+  unsigned char* prev_element_adress = array_adress;
+  unsigned char* current_element_adress = array_adress+1;
+  for ( unsigned char i = 1 ; i <= (array_length-1)  ; i++ )
+  {
+    prev_element_adress = array_adress;
+    current_element_adress = array_adress+1;
 
-for ( unsigned char i = 1 ; i <= (array_length-1) ; i++ )
-{
-if ( *prev_element_adress < *current_element_adress )
-exchange_two_variables( prev_element_adress, current_element_adress );
+    for ( unsigned char i = 1 ; i <= (array_length-1) ; i++ )
+    {
+      if ( *prev_element_adress < *current_element_adress )
+        exchange_two_variables( prev_element_adress, current_element_adress );
 
-prev_element_adress++;
-current_element_adress++;
+      prev_element_adress++;
+      current_element_adress++;
 
-}
-}
+    }
+  }
 }
 
 
 void exchange_two_variables(unsigned char* x, unsigned char* y)
 {
-*x = *x + *y;
-*y = *x - *y;
-*x = *x - *y;
+  *x = *x + *y;
+  *y = *x - *y;
+  *x = *x - *y;
 }
